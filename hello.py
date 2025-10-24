204003b0cbfdec6ba8066d9eb8d2746c4344fa9e
Ainazik111 <ainazikmirlanova159@gmail.com>
2025-10-24 21:13:52 +0600

    friday commit

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print(2+2)
+# myname = 'Bob'
+# myName = 'Alise'
+# my_name = 'Eve'
+# sales_price = 19.99
+# print(5 >= 2)
+
+a = input('Enter your name:')
+print('Hello', a)
---
+++ thirdLesson.py
@@ -0,0 +1,89 @@
+# name = input("Enter your name: ")  # there i enter name
+
+# '''
+
+# fkvjrubhrv
+# rgrtg
+# rge
+# это текст 
+# '''
+
+# num = 3 
+# num1, num2, num3 = 4, 5, 6
+# print(num3)
+# num1 = 4 
+# num2 = 5 
+# num3 = 6  # как можно давать значение переменным
+
+
+# лист  Списки (list) - изменяемые последовательности
+
+# products = ['milk', 'bread', 'eggs']
+# print(products[1])
+# print(products [2]) # первый способ как можем получить яйцо
+# print(products[-1])
+
+# #ex 1 i am doing
+ 
+# a = ['water', 'Ainazik', 3, 4.55, 'nute', 'apple', 'market', 'lemon', 'bottle', 120]
+# # print('i want to by:', a [4])
+# # print(f'costs {a[3]}')
+
+# element = input("What do you want to buy? ")
+# print(element,f'costs {a[2]} som')
+
+# price = int(input("Enter price: "))
+# print("Change is", 500-price )
+
+# print("Change is", 500-int(price )) 
+
+# print(f'Iwant to by {products}')
+
+# products2 = list()
+
+# Параметры print()
+# print('hello', 34, 899.999, sep = '\n', end = '*')
+
+
+# арифметические действия 
+
+num = 5 / 2 
+print(num)
+
+num2 = 5 // 2 
+print(num2)
+
+num3 = 101 % 10 
+print(num3)
+
+num4 = 3 * 3
+print(num4)
+
+num5 = 3 ** 2 
+print(num5)
+
+# примеры
+import  math as mt
+
+print(5 * 10 + 2) 
+
+print(2/34 + ((23/46)**2))
+
+print(2 * (3 - 1))
+
+print((5-1)*(5+1))
+
+print(0.3*(4-1))
+
+print((91-1)/(2+1))
+
+
+print(mt.sqrt((5*4)/3))
+
+print(((5*4)/3)**0.5)
+
+print(1/3 + 5 * ((0.2 - 0.001)/91))
+
+print((40-((4.25 * 7.08) + (6.768 / 0.75))) * 2050)
+
+print(0.16*(3.2 - 3/40) + (2*(3/11)*4.125 / 3*3/4)/())
